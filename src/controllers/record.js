c7acc2b8b2d9cbaebd5dc7e751e3f01bd1e41b2f
slycloud <sky_zuoyou@sina.com>
2014-06-27 11:59:22 +0800

    add controllers and controllers' test
    add controllers and test for dashboard, dataSource, widget, record

---
+++ src/controllers/record.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var router = require('express').Router();
+module.exports = router;
+
+var bodyParser = require('body-parser');
+var Project = require('../logicals/project');
+var DataSource = require('../logicals/dataSource');
+var Record = require('../logicals/record');
+
+router.post(
+    '/api/projects/:uuid/data_sources/:key',
+    bodyParser.json(),
+    function(req, res, next){
+        var dataSourceId = null;
+        var record = req.body;
+        if(!record.value){
+            res.send(400);
+        }
+
+        DataSource.getByUUIDAndKey(req.param('uuid'), req.param('key')).then(function (dataSource) {
+            if (!dataSource) {
+                return res.send(404);
+            }
+
+            record.data_source_id = dataSource.id;
+            return Record.save(record).then(function (id) {
+                return Record.get(id);
+            }).then(function (record) {
+                res.send(record);
+            });
+        }).catch(next);
+    }
+);
---
+++ src/controllers/widget.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var router = require('express').Router();
+module.exports = router;
+
+var bodyParser = require('body-parser');
+var Dashboard = require('../logicals/dashboard');
+var Widget = require('../logicals/widget');
+
+router.get(
+    '/api/dashboards/:dashboardId/widgets',
+    function(req, res, next){
+        var dashboardId = parseInt(req.param('dashboardId'), 10);
+        Widget.find({
+            dashboard_id: dashboardId
+        }).then(function(widgets){
+            res.send(widgets);
+        }).catch(next);
+    }
+);
+
+router.get(
+    '/api/dashboards/:dashboardId/widgets/:id',
+    function(req, res, next){
+        var id = parseInt(req.param('id'), 10);
+
+        Widget.get(id).then(function(widget) {
+            if (!widget) {
+                return res.send(404);
+            }
+
+            res.send(widget);
+        }).catch(next);
+    }
+);
+
+router.post(
+    '/api/dashboards/:dashboardId/widgets',
+    bodyParser.json(),
+    function(req, res, next) {
+        var dashboardId = parseInt(req.param('dashboardId'), 10);
+        var widget = req.body;
+
+        Dashboard.get(dashboardId).then(function (dashboard) {
+            if(!dashboard){
+                return res.send(400);
+            }
+        });
+
+        if (!widget.dashboard_id || !widget.type || !widget.config) {
+            res.send(400);
+        }
+
+        Widget.save(widget).then(function(id){
+            return Widget.get(id);
+        }).then(function(widget){
+            res.send(widget);
+        }).catch(next);
+    }
+);
+
+router.put(
+    '/api/dashboards/:dashboardId/widgets/:id',
+    bodyParser.json(),
+    function(req, res, next) {
+        var widget = req.body;
+        if(!widget.type && !widget.config){
+            res.send(400);
+        }
+
+        var id = parseInt(req.param('id'), 10);
+
+        Widget.update(id, widget).then(function(){
+            return Widget.get(id);
+        }).then(function(widget){
+            res.send(widget);
+        }).catch(next);
+    }
+);
+
+router.delete(
+    '/api/dashboards/:dashboardId/widgets/:id',
+    function(req, res, next) {
+        var id = parseInt(req.param('id'), 10);
+
+        Widget.remove(id).then(function(){
+            res.send(200);
+        }).catch(next);
+    }
+);
