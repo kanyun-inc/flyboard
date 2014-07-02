dfd0fd015eabb6e2c758f12e48d0235f346d1a2a
perfectworks <perfectworks@gmail.com>
2014-07-02 15:26:31 +0800

    render dashboards from database

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,14 +4,41 @@ var router = require('express').Router();
 module.exports = router;
 
 var Widget = require('../logicals/widget');
+var Dashboard = require('../logicals/Dashboard');
+var Promise = require('bluebird');
 
-router.get('/', function (req, res, next) {
-    var dashboardId = 1;
+function indexCtrl(req, res, next) {
+    var dashboards = Dashboard.find();
+    var dashboard = req.param('dashboardId') ?
+        Dashboard.get(parseInt(req.param('dashboardId'), 10)) :
+        dashboards.then(function (dashboards) {
+        if (!dashboards || dashboards.length === 0) {
+            return null;
+        }
 
-    Widget.find({dashboard_id: dashboardId}).then(function (widgets){
-        res.render('index', {
-            title: 'Flyboard',
-            widgets: widgets
+        return dashboards[0];
+    });
+
+    var widgets = dashboard.then(function (dashboard) {
+        if (!dashboard) {
+            return [];
+        }
+
+        return Widget.find({
+            dashboard_id: dashboard.id
         });
+    });
+
+    res.locals.title = 'Flyboard';
+
+    Promise.props({
+        dashboards: dashboards,
+        dashboard: dashboard,
+        widgets: widgets
+    }).then(function (result) {
+        res.render('index', result);
     }).catch(next);
-});
+}
+
+router.get('/', indexCtrl);
+router.get('/dashboards/:id', indexCtrl);
