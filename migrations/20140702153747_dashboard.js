1beb2710c873f3cce07b105aa6374037a8775470
perfectworks <perfectworks@gmail.com>
2014-07-02 16:33:34 +0800

    update migrate

--- migrations/20140702153747_dashboard.js
+++ migrations/20140702153747_dashboard.js
@@ -3,7 +3,7 @@
 exports.up = function (knex, Promise) {
     return Promise.all([
         knex.schema.table('dashboards', function (table) {
-            table.json('config').notNullable().defaultTo('{}');
+            table.json('config');
         })
     ]);
 };
