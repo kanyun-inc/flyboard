f725790501ae53b292354e7b8a4ba3320bcbf153
slycloud <sky_zuoyou@sina.com>
2014-07-04 18:36:33 +0800

    correct index.js: delete unused "next"

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -43,6 +43,6 @@ function indexCtrl(req, res, next) {
 router.get('/', indexCtrl);
 router.get('/dashboards/:id', indexCtrl);
 
-router.get('/admin', function (req, res, next) {
+router.get('/admin', function (req, res) {
    res.render('admin');
 });
