034d6cac7deb2505f94e1a8a5937eab5b1937827
slycloud <sky_zuoyou@sina.com>
2014-07-10 19:00:54 +0800

    delete angular-dragdrop.js in staticFile.js

--- src/middlewares/staticFile.js
+++ src/middlewares/staticFile.js
@@ -8,7 +8,6 @@ var staticFiles = {
         'bower_components/jquery/dist/jquery.min.js',
         'bower_components/jquery-ui/jquery-ui.js',
         'bower_components/angular/angular.min.js',
-        'bower_components/angular-dragdrop/src/angular-dragdrop.min.js',
         'bower_components/angular-route/angular-route.min.js',
         'bower_components/angular-resource/angular-resource.min.js',
         'bower_components/angular-resource/angular-resource.min.js',
