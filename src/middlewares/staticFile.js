09cee27930b70deccd89db08750891cf5bc254cb
slycloud <sky_zuoyou@sina.com>
2014-07-10 19:04:31 +0800

    fix problem caused by deleting angular-dragdrop

--- src/middlewares/staticFile.js
+++ src/middlewares/staticFile.js
@@ -5,13 +5,13 @@ var Promise = require('bluebird');
 
 var staticFiles = {
     js: [
-        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/jquery/dist/jquery.js',
         'bower_components/jquery-ui/jquery-ui.js',
-        'bower_components/angular/angular.min.js',
-        'bower_components/angular-route/angular-route.min.js',
-        'bower_components/angular-resource/angular-resource.min.js',
-        'bower_components/angular-resource/angular-resource.min.js',
-        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/angular-resource/angular-resource.js',
+        'bower_components/angular-resource/angular-resource.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
         'controlfrog/js/chart.js',
 
         'controlfrog/js/easypiechart.js',
