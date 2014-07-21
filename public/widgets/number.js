ba89d86d15b2bf0cc62728070d548e322b2bde36
slycloud <sky_zuoyou@sina.com>
2014-07-21 18:39:50 +0800

    Mod: Number widget Refresh && Widget CSS Style
    mode css style of Widget: Number && Pie

--- public/widgets/number.js
+++ public/widgets/number.js
@@ -2,6 +2,9 @@
  * Created by sly on 14-6-27.
  */
 
+function getTimeFromRecord(record) {
+    return new Date(record.year, record.month - 1, record.day, record.hour, record.minute, record.second).getTime();
+}
 // Initialise sparklines
 /*
  *	Copy the each() function for each sparkline you have
@@ -9,75 +12,106 @@
  */
 $(function () {
     $('.widget[data-type=1]').each(function () {
-        var $widget = $(this);
-        var $metric = $widget.find('.metric');
-        var $metricSmall = $widget.find('.metric-small');
-        var $arrow = $widget.find('.arrow');
-        var $large = $widget.find('.large');
-        var $small = $widget.find('.small');
         var config = $(this).data('config');
-        var $sparkline = $(this).find('.sparkline');
+        var latestRecordId = undefined;
 
-        /*
-         // Set custom options and merge with default
-         customSparkOptions = {};
-         customSparkOptions.minSpotColor = true;
-         var sparkOptions = cf_defaultSparkOpts;
-         var sparkOptions = $.extend({}, cf_defaultSparkOpts, customSparkOptions);
-         */
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
 
-        // No custom options
-        var sparkOptions = cf_defaultSparkOpts;
+        var $container = $(this).find('.content');
 
-        var data;
+        $.get(
+            '/api/data_sources/' + config.dataSourceId + '/records?limit=' + config.limit,
+            function (resp) {
+                var data = [];
+                resp = resp || [];
 
-        function reload(){
-            $.get(
-                '/api/data_sources/' + config.dataSourceId + '/records?limit=' + config.limit,
-                function (resp) {
-                    data = [];
-                    resp.reverse().forEach(function (record) {
-                        data.push(record.value);
+                latestRecordId = resp[0].id;
+                resp.reverse().forEach(function(record){
+                    data.push({
+                        x: getTimeFromRecord(record),
+                        y: record.value
                     });
+                });
 
-                    var lastValue = data[data.length-2] || 0;
-                    var det;
-                    if(lastValue !== 0){
-                        det = ((data[data.length-1] - lastValue)/lastValue) * 100;
-                    }
-                    else{
-                        det = 0;
-                    }
+                $container.highcharts({
+                    chart: {
+                        backgroundColor: '#2b2b2b',
+                        type: 'spline',
+                        animation: Highcharts.svg, // don't animate in old IE
+                        marginRight: 10,
+                        events: {
+                            load: function() {
+                                // set up the updating of the chart each second
+                                var series = this.series[0];
+                                setInterval(function() {
+                                    $.get(
+                                            '/api/data_sources/' + config.dataSourceId + '/records?limit=' + config.limit,
+                                        function(resp) {
+                                            var flag = false;
+                                            resp.reverse().filter(function(record){
+                                                if(record.id === latestRecordId){
+                                                    flag = true;
+                                                    return false;
+                                                }
 
-                    var value = data[data.length - 1];
-                    $metric.html(value);
-                    if( det > 0 ){
-                        $arrow.removeClass('arrow-down');
-                        $arrow.addClass('arrow-up');
-                        $metricSmall.removeClass('m-green');
-                        $metricSmall.addClass('m-red');
-                    }
-                    else if(det < 0){
-                        $arrow.removeClass('arrow-up');
-                        $arrow.addClass('arrow-down');
-                        $metricSmall.removeClass('m-red');
-                        $metricSmall.addClass('m-green');
-                    }
-                    else{
-                        $arrow.removeClass('arrow-up arrow-down');
-                        $metricSmall.removeClass('m-red');
-                        $metricSmall.addClass('m-green');
-                    }
-                    det = Math.abs(det).toFixed(2).toString().split('.');
-                    $large.text(det[0]);
-                    $small.text('.' + det[1] + '%');
-                    createSparkline($sparkline, data, sparkOptions);
-                },
-                'json'
-            );
-        }
 
-        setInterval(reload, config.reloadInterval);
-        reload();
+                                                if (flag) {
+                                                    series.addPoint([getTimeFromRecord(record), record.value], true, true);
+                                                }
+                                            });
+
+                                            latestRecordId = resp[resp.length - 1].id;
+                                    });
+                                }, config.reloadInterval);
+                            }
+                        }
+                    },
+                    title: {
+                        text: config.name
+                    },
+                    xAxis: {
+                        type: 'datetime',
+                        tickPixelInterval: 150
+                    },
+                    yAxis: {
+                        title: {
+                            text: ''
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                    },
+                    tooltip: {
+                        formatter: function() {
+                                return '<b>'+ this.series.name +'</b><br/>'+
+                                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                                Highcharts.numberFormat(this.y, 2);
+                        }
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    exporting: {
+                        enabled: false
+                    },
+                    series: [{
+                        name: config.name,
+                        data: data,
+                        color: '#46BFBD'
+                    }],
+                    plotOptions:{
+                        spline: {
+                            colors: ['#46BFBD', '#F7464A', '#FDB45C', '#949FB1', '#4D5360']
+                        }
+                    }
+                });
+            }
+        );
     });
 });
