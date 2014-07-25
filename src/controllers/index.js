a56ab2e93d0d6bc61a5c42e7b0fb9a5738291be5
slycloud <sky_zuoyou@sina.com>
2014-07-25 18:54:23 +0800

    Add: stat data list

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,6 +6,7 @@ module.exports = router;
 var Widget = require('../logicals/widget');
 var Dashboard = require('../logicals/dashboard');
 var DataSource = require('../logicals/dataSource');
+var Record = require('../logicals/record');
 var Promise = require('bluebird');
 
 function indexCtrl(req, res, next) {
@@ -51,8 +52,7 @@ function statCtrl(req, res, next){
     widget.config = {
         name: '',
         dataInfos: [],
-        reloadInterval: 600000,
-        limit: 1000
+        reloadInterval: 600000
     };
     var dataSources = DataSource.find().then(function(dataSources) {
         if (!dataSources || dataSources.length === 0) {
@@ -68,20 +68,39 @@ function statCtrl(req, res, next){
             var ids = req.param('dataSourceIds').split(',');
             return ids || [];
         }()) : [];
-    dataSourceIds.forEach(function(id){
+
+    var period = req.param('period') || 300;
+
+    var multiRecords = Promise.map(dataSourceIds, function(id){
         widget.config.dataInfos.push({
             id: parseInt(id, 10)
         });
-    });
 
-    var period = req.param('period') || 300;
+        return DataSource.get(id).then(function(dataSource) {
+            return Record.find({
+                data_source_id: id
+            }).then(function(records){
+                return {
+                    name: dataSource.name,
+                    dataSourceId: id,
+                    data: records
+                };
+            });
+        });
+    }).then(function(results){
+        // console.log(results);
+        return results;
+    });
+    console.log('xxx');
 
     res.locals.title = 'Status';
     Promise.props({
         widget: widget,
         dataSources: dataSources,
-        period: period
+        period: period,
+        multiRecords: multiRecords
     }).then(function (result) {
+        console.log(result.multiRecords);
         res.render('stat', result);
     }).catch(next);
 }
