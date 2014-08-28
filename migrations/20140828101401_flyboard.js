a2b686caa7516a813f0bf6b1f3000ad42cebf42c
slycloud <sky_zuoyou@sina.com>
2014-08-28 19:19:15 +0800

    Mod: database for multi datasources

---
+++ migrations/20140828101401_flyboard.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.up = function(knex) {
+    return knex.schema.table('data_sources', function (table) {
+        table.json('config');
+    }).then(function () {
+        return knex.schema.table('records', function (table) {
+            table.string('dim1');
+            table.string('dim2');
+        });
+    });
+};
+
+exports.down = function(knex, Promise) {
+
+};
