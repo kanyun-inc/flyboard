753321c139a6c7a14aa8f701f7a22b8a6b82bfe9
perfectworks <perfectworks@gmail.com>
2014-09-25 11:12:41 +0800

    ADD: increment data

---
+++ migrations/20140925100254_flyboard.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.up = function (knex, Promise) {
+    return Promise.all([
+            knex.schema.table('data_sources', function (table) {
+                table.boolean('increment');
+            })
+        ]);
+};
+
+exports.down = function () {
+};
