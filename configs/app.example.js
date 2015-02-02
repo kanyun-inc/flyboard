6e959081f1b7dc58c1063b877ac8ac28db4915b3
slycloud <sky_zuoyou@sina.com>
2015-02-02 16:56:39 +0800

    ADD: local strategy passport

--- configs/app.example.js
+++ configs/app.example.js
@@ -1,55 +1,125 @@
 'use strict';
 
+// Passport Configuration
+//
+// authItems support multiple strategies.
+//
+// ```javascript
+// exports.authItems = [
+//     {
+//         key: 'local',
+//         keyName: 'Local',
+//         authUrl: '/auth/local',
+//         returnUrl: '/auth/local/return'
+//     },
+//     {
+//         key: 'google',
+//         keyName: 'Google',
+//         ...
+//     }
+// ];
+// ```
+
 var passport = require('passport');
-var GoogleStrategy = require('passport-google').Strategy;
 var User = require('../src/logicals/user');
-var blueBird = require('bluebird');
 
-passport.use(new GoogleStrategy({
-    returnURL: 'http://127.0.0.1:3000/auth/google/return',
-    realm: 'http://127.0.0.1:3000/'
-}, function (identifier, profile, done) {
-    var email = null;
+/*
+ * local passport example
+ */
+var LocalStrategy = require('passport-local').Strategy;
 
-    if (profile && profile.emails && profile.emails.length) {
-        email = profile.emails[0].value;
-    }
-
-    //err handle
-    if (!email) {
-        done(new Error('invalid email'));
-    }
-
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, function(email, password, done) {
+    // find user, if user doesn't exist, create
     User.findOrCreate({
         email: email
-    }).then(function (user) {
+    }).then(function (user){
         done(null, user);
     }).catch(done);
-}));
+  }
+));
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
     done(null, user.email);
 });
 
-passport.deserializeUser(function (email, done) {
-    blueBird.resolve(User.findOne({
+passport.deserializeUser(function(email, done) {
+    User.findOne({
         email: email
-    })).then(function (user) {
-        if (!user) {
-            var err = new Error('invalid session');
+    }).then(function (user){
+        if(!user){
+            var err = new Error('invalid user');
             err.status = 403;
-            done(err, user);
-            return;
+
+            return done(err, user);
         }
 
-        done(null, user);
+        return done(null, user);
     }).catch(done);
 });
 
 exports.passport = passport;
-exports.authItems = [{
-    key: 'google',
-    keyName: 'Google',
-    authUrl: '/auth/google',
-    returnUrl: '/auth/google/return'
-}];
+exports.authItems = [
+    {
+        key: 'local',
+        keyName: 'Local',
+        authUrl: '/auth/local',
+        returnUrl: '/auth/local/return'
+    }
+];
+
+/*
+* google passport example
+*/
+//var GoogleStrategy = require('passport-google').Strategy;
+//
+//passport.use(new GoogleStrategy({
+//    returnURL: 'http://127.0.0.1:3000/auth/google/return',
+//    realm: 'http://127.0.0.1:3000/'
+//}, function (identifier, profile, done) {
+//    var email = null;
+//
+//    if (profile && profile.emails && profile.emails.length) {
+//        email = profile.emails[0].value;
+//    }
+//
+//    //err handle
+//    if (!email) {
+//        done(new Error('invalid email'));
+//    }
+//
+//    User.findOrCreate({
+//        email: email
+//    }).then(function (user) {
+//        done(null, user);
+//    }).catch(done);
+//}));
+//
+//passport.serializeUser(function (user, done) {
+//    done(null, user.email);
+//});
+//
+//passport.deserializeUser(function (email, done) {
+//    blueBird.resolve(User.findOne({
+//        email: email
+//    })).then(function (user) {
+//        if (!user) {
+//            var err = new Error('invalid session');
+//            err.status = 403;
+//            done(err, user);
+//            return;
+//        }
+//
+//        done(null, user);
+//    }).catch(done);
+//});
+//
+//exports.passport = passport;
+//exports.authItems = [{
+//    key: 'google',
+//    keyName: 'Google',
+//    authUrl: '/auth/google',
+//    returnUrl: '/auth/google/return'
+//}];
